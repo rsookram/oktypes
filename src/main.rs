a1c1b9f8ad2745f17ffbfdbf8e9596f194cb3336
rsookram <rashadsookram@gmail.com>
2021-08-24 22:00:19 -0400

    Implement initial working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use tree_sitter::{Language, Parser, Query, QueryCursor};
+
+extern "C" {
+    fn tree_sitter_kotlin() -> Language;
+}
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: Add safety comment
+    let language = unsafe { tree_sitter_kotlin() };
+
+    let mut parser = Parser::new();
+    // TODO: Handle errors
+    parser.set_language(language).unwrap();
+
+    // TODO: Query for typealiases too
+    let query = Query::new(language, "(class_declaration) @class").unwrap();
+
+    // TODO: Parallelize
+    for arg in std::env::args_os().skip(1) {
+        let mut cursor = QueryCursor::new();
+
+        let source = std::fs::read_to_string(&arg).unwrap();
+        let tree = parser.parse(&source, None).unwrap();
+
+        let matches = cursor.captures(&query, tree.root_node(), |_| "");
+
+        for (m, _) in matches {
+            let node = m.captures.first().unwrap().node;
+            let id_node = node.child_by_field_name("identifier").unwrap();
+
+            // TODO: lock stdout manually
+            println!(
+                "{}:{} {}",
+                arg.to_string_lossy(),
+                id_node.start_position().row,
+                &source[id_node.byte_range()]
+            );
+        }
+    }
 }
